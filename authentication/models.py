e2cf987405dcda1fd746eb5d11bc243b88cdefde
Martin Freytes <martin.jfreytes@gmail.com>
2015-01-18 20:32:42 -0500

    Installing and migrating authentication app

--- authentication/models.py
+++ authentication/models.py
@@ -3,33 +3,6 @@ from django.contrib.auth.models import BaseUserManager
 from django.db import models
 
 
-class Account(AbstractBaseUser):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=40, unique=True)
-
-    first_name = models.CharField(max_length=40, blank=True)
-    last_name = models.CharField(max_length=40, blank=True)
-    tagline = models.CharField(max_length=140, blank=True)
-
-    is_admin = models.BooleanField(default=False)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    objects = AccountManager()
-
-    USERNAME_FIELD ='email'
-    REQUIRED_FIELDS = ['username']
-
-    def __unicode__(self):
-        return self.email
-
-    def get_full_name(self():
-        return ' '.join([self.first_name, self.last_name])
-        
-    def get_short_name(self):
-        return self.first_name
-
 class AccountManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
         if not email:
@@ -54,3 +27,31 @@ class AccountManager(BaseUserManager):
         account.save()
 
         return account
+
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
+
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+    tagline = models.CharField(max_length=140, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD ='email'
+    REQUIRED_FIELDS = ['username']
+
+    def __unicode__(self):
+        return self.email
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+        
+    def get_short_name(self):
+        return self.first_name
